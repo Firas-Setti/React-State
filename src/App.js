0addfb236ae46339d0d2d3760012e5945097401e
Firas-Setti <setti.firas@gmail.com>
2022-02-28 07:53:58 +0100

    State Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import klaus  from './klaus.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      show : true,
+      Person : { 
+                fullName : "Firas Setti",
+                bio : "I am a fresh graduate student who got his Masters degree in Digital Marketing and Data Analytics, and his bachelors degree in Business English. Nowadays, I am studying at GoMyCode to become a FullStack Web Developer", 
+                imgSrc: klaus, 
+                profession:"FullStack Web Developement Student"
+              },
+    }
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+  
+
+
+  clickHandler(){
+    this.setState(function (prevState){
+      return{
+        show : !prevState.show ,
+      }
+    });
+  }
+
+  render(){
+    if(this.state.show)
+    return (
+      <div className="container">
+        <button className="btn btn-primary" onClick={this.clickHandler}>Hide</button>
+        <br/>
+        <img src={klaus} alt="image peronel"/>
+        <h1>{this.state.Person.fullName}</h1>
+        <h2>{this.state.Person.bio}</h2>
+        <h2>{this.state.Person.profession}</h2>
+      </div>
+      );
+      else return (
+        <div className="container">
+      <button className="btn btn-primary" onClick={this.clickHandler}>Show</button>
+      </div>
+      );
+  }
 }
 
 export default App;
